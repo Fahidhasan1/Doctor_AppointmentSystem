b997b6a61bbcfee338575a91c86f79774610799c
Fahidhasan1 <mdfahidhasan72@gmail.com>
2025-11-29 18:53:06 +0600

    Add project files.

---
+++ Doctor_AppointmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Doctor_AppointmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
